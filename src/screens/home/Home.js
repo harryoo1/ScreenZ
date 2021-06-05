f215ff58d0fd488b7623180cc50c89fe6deb897d
harryooo1 <harry94106@gmail.com>
2021-06-05 22:56:52 +0530

    Added Upcoming Movies heading on home page & added local movie data file

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
 import './home.css';
 import Header from '../../common/header/Header';
+import {withStyles} from '@material-ui/core/styles';
+import moviesData from '../../common/movieData';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    upcomingMoviesHeading: {
+        textAlign: 'center',
+        background: 'rgb(124, 142, 248)',
+        padding: '8px',
+        color: '#fff',
+        fontWeight: 500,
+        fontSize: '1.25rem'
+    },
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    }
+});
 
 class Home extends Component{
     render(){
+        const {classes} = this.props;
         return(
             <div>
                 <Header/>
+                <div className={classes.upcomingMoviesHeading}>
+                <span>Upcoming Movies</span>
+                </div>
 
             </div>
         )
     }
 }
-export default Home;
+export default withStyles(styles)(Home);
